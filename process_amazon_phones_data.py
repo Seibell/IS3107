660c8d22b08eb4054ff35059d476accfdeede6d2
Ong Chang Hui <97486532+is-changhui@users.noreply.github.com>
2023-04-17 21:40:36 +0800

    Add files via upload

--- process_amazon_phones_data.py
+++ process_amazon_phones_data.py
@@ -10,12 +10,12 @@ import nltk
 import nltk.tokenize
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
+
 def process_data():
     # Connect to MongoDB
     client = MongoClient('localhost', 27017)
-    mydb = client["IS3107Project"]
-    my_collection = mydb["phonesNaccessories_filtered"]
-
+    mydb = client["Cell_Phones"]
+    my_collection = mydb["products"]
 
     # Filtering dataset to remove unwanted elements
     result = mydb.phones.aggregate([
@@ -35,60 +35,94 @@ def process_data():
     record = result.next()
 
     print(record)
+    # Aggregate Queries
+    # Remove all irrelevant fields
+    q0 = {"$unset": ["fit", "tech1", "tech2", "imageURL", "imageURLHighRes"]}
 
+# Match all documents that have "Cell Phones" string in category field
+    q1 = {"$match": {"category": {"$in": ["Cell Phones"]}}}
 
-    joinQ = {"$lookup": {"from": "reviews","localField": "asin","foreignField": "asin","as": "result"}}
-    aggPipeWithJoin = [q0, q1, q2, q3, q3p1, q3p2, q4, q5, sortQ, joinQ]
+# Match all documents that have some sort of ranking in the general Cell Phones & Accessories
+    regexQ2 = re.compile(
+        "^>#.* in Cell Phones & Accessories \(See Top 100 in Cell Phones & Accessories\)")
+    q2 = {"$match": {"rank": {"$elemMatch": {"$regex": regexQ2}}}}
+
+# Add a new field ranking the documents based on the "rank" string. Removes all hashtags and commas.
+    regexQ3 = re.compile("[#,]")
+    q3 = {"$addFields": {"overallRankRemovedHash": {"$replaceAll": {
+        "input": {"$arrayElemAt": ["$rank", 0]}, "find": "#", "replacement": ""}}}}
+
+    q3p1 = {"$addFields": {"overallRankRemovedRightArrow": {"$replaceAll": {
+        "input": "$overallRankRemovedHash", "find": ">", "replacement": ""}}}}
+    q3p2 = {"$addFields": {"overallRankRemovedComma": {"$replaceAll": {
+        "input": "$overallRankRemovedRightArrow", "find": ",", "replacement": ""}}}}
+
+# Now split by whitespace and only select first element (which is the number rank)
+    q4 = {"$addFields": {"overallRankNumber": {"$toInt": {
+        "$arrayElemAt": [{"$split": ["$overallRankRemovedComma", " "]}, 0]}}}}
+
+# remove the intermediate "rankRemovedHashComma" (and also _id actually)
+    q5 = {"$unset": ["overallRankRemovedHash",
+                     "overallRankRemovedRightArrow", "overallRankRemovedComma", "_id"]}
+
+# sort by rank ascending
+    sortQ = {"$sort": {"overallRankNumber": 1}}
+
+    # joinQ = {"$lookup": {"from": "reviews", "localField": "asin",
+    #                      "foreignField": "asin", "as": "result"}}
+    aggPipeWithJoin = [q0, q1, q2, q3, q3p1, q3p2, q4, q5, sortQ]
     commandCursorJoined = my_collection.aggregate(aggPipeWithJoin)
-
 
     documentsJoined = []
     for document in commandCursorJoined:
         documentsJoined.append(document)
-        
+
+# pass on to next dag
     with open('outputJoined.json', 'w') as outfile:
         json.dump(documentsJoined, outfile)
 
+    # result = mydb.phones.aggregate([
+    #     {
+    #         "$lookup": {
+    #             "from": "review5core",
+    #             "localField": "asin",
+    #             "foreignField": "asin",
+    #             "as": "reviewList"
+    #         }
+    #     },
+    #     {
+    #         "$project": {
+    #             "_id": 0,
+    #             "asin": 1,
+    #             "title": 1,
+    #             "brand": 1,
+    #             "feature": 1,
+    #             "price": 1,
+    #             "category": 1,
+    #             "overallRankNumber": 1,
+    #             "reviewList": 1
+    #         }
+    #     }
+    # ])
 
-    result = mydb.phones.aggregate([
-    {
-        "$lookup": {
-        "from": "review5core",
-        "localField": "asin",
-        "foreignField": "asin",
-        "as": "reviewList"
-        }
-    },
-    {
-        "$project": {
-        "_id": 0,
-        "asin": 1,
-        "title": 1,
-        "brand": 1,
-        "feature": 1,
-        "price": 1,
-        "category": 1,
-        "overallRankNumber": 1,
-        "reviewList": 1
-        }
-    }
-    ])
+    # record = result.next()
 
-    record = result.next()
+    # print(record)
 
-    print(record)
-
-    joinQ = {"$lookup": {"from": "review5core","localField": "asin","foreignField": "asin","as": "reviewList"}}
+    joinQ = {"$lookup": {"from": "review5core", "localField": "asin",
+                         "foreignField": "asin", "as": "reviewList"}}
     aggPipeWithJoin = [joinQ]
     commandCursorJoined = my_collection.aggregate(aggPipeWithJoin)
-
 
     documentsJoined = []
     for document in commandCursorJoined:
         documentsJoined.append(document)
 
-    documentsJsonString = json.dumps(documentsJoined, default=json_util.default)
+    documentsJsonString = json.dumps(
+        documentsJoined, default=json_util.default)
     # # write list of dicts to file
+
+# pass on to next dag
     with open('outputJoined.json', 'w') as outfile:
         outfile.write(documentsJsonString)
 
@@ -97,20 +131,19 @@ def process_data():
     joinedSampleCollection = mydb["cleanedReviewJoined"]
 
     testDocument = joinedSampleCollection.find_one()
-    #print(testDocument.items())
+    # print(testDocument.items())
 
     # Test on first review
     testReview = testDocument['reviewList'][1]
     # print(testReview)
+
     def addNounPhrasesCountField(review):
-        
+
         review['nounPhrasesCount'] = {}
 
-        
         if 'reviewText' not in review:
             return
-        
-        
+
         testReviewText = review['reviewText']
 
         # Tokenising sentence
@@ -124,8 +157,7 @@ def process_data():
     #     gram = r"""NP: {<RB.?>*<VB.?>*<NNP>+<NN>?}""" # more specific & effective noun phrase identifier
 
         gram = r"NP: {<DT|PRP\$>?<JJ>*<NN.*>+}"
-        
-        
+
         chunkParser = nltk.RegexpParser(gram)
         parsed_tree = chunkParser.parse(tagged_tokens)
 
@@ -137,11 +169,11 @@ def process_data():
         #     for leaf in tree.leaves():
         #         print(leaf)
 
-
         # each Leaf object contains [0] the word; and [1] the word category (eg. "something / NN")
 
-        noun_phrases = [ ' '.join(leaf[0] for leaf in tree.leaves()).lower() #have to lower here
-                        for tree in parsed_tree.subtrees() #        idk why if i lower earlier the parser fks up
+        noun_phrases = [' '.join(leaf[0] for leaf in tree.leaves()).lower()  # have to lower here
+                        # idk why if i lower earlier the parser fks up
+                        for tree in parsed_tree.subtrees()
                         if tree.label() == 'NP']
 
     #     print(noun_phrases)
@@ -157,7 +189,7 @@ def process_data():
 
     # print(testReview)
 
-    #test on review 18 (looking for some normal output)
+    # test on review 18 (looking for some normal output)
     reviewThree = testDocument['reviewList'][17]
 
     addNounPhrasesCountField(reviewThree)
@@ -167,7 +199,8 @@ def process_data():
     def gatherAllReviewsNPCount(productDocument, howMany=0):
         productReviews = productDocument['reviewList']
         productDocument['totalNounPhrasesCount'] = {}
-        productDocumentCount = productDocument['totalNounPhrasesCount'] # just to make it shorter
+        # just to make it shorter
+        productDocumentCount = productDocument['totalNounPhrasesCount']
     #     for rev in productReviews:
 
     #     doneCount = 0
@@ -175,22 +208,24 @@ def process_data():
             for x in range(howMany):
                 rev = productReviews[x]
                 addNounPhrasesCountField(rev)
-                #test and do for first 4 first
+                # test and do for first 4 first
 
                 for np in rev['nounPhrasesCount']:
-                    productDocumentCount[np] = productDocumentCount.get(np, 0) + rev['nounPhrasesCount'][np]
+                    productDocumentCount[np] = productDocumentCount.get(
+                        np, 0) + rev['nounPhrasesCount'][np]
         else:
             for rev in productReviews:
-    #             print("doing else once for " + str(doneCount))
-    #             doneCount += 1
+                #             print("doing else once for " + str(doneCount))
+                #             doneCount += 1
                 addNounPhrasesCountField(rev)
 
                 for np in rev['nounPhrasesCount']:
-                    productDocumentCount[np] = productDocumentCount.get(np, 0) + rev['nounPhrasesCount'][np]
-        
+                    productDocumentCount[np] = productDocumentCount.get(
+                        np, 0) + rev['nounPhrasesCount'][np]
+
     #     print(productDocument)
 
-    #print(testDocument)
+    # print(testDocument)
     gatherAllReviewsNPCount(testDocument)
     # print(testDocument)
 
@@ -200,96 +235,96 @@ def process_data():
     for document in cursor:
         gatherAllReviewsNPCount(document)
         documents.append(document)
-        
 
-
-    ## outputting    
+    # outputting
     documentsJsonString = json.dumps(documents, default=json_util.default)
     # # write list of dicts to file
+
+    # pass on to next dag
     with open('outputWithNPCount.json', 'w') as outfile:
         outfile.write(documentsJsonString)
 
     def addSentimentToReview(review, lyzer):
 
-
         if 'reviewText' not in review:
-            return # have to account for this later
-        
-        
+            return  # have to account for this later
+
         testReviewText = review['reviewText']
 
         score = lyzer.polarity_scores(testReviewText)
-        
-        review['sentiment'] = score 
-        
 
-    #before
-    #print(testReview)
+        review['sentiment'] = score
+
+    # before
+    # print(testReview)
 
     lyzer = SentimentIntensityAnalyzer()
     addSentimentToReview(testReview, lyzer)
 
-    #after
-    #print(testReview)
+    # after
+    # print(testReview)
 
     def generateSentiment(productDocument, lyzer):
         reviewsList = productDocument['reviewList']
-        
-        averageSentiment = {'neg': 0.0, 'neu': 0.0, 'pos': 0.0, 'compound': 0.0}
+
+        averageSentiment = {'neg': 0.0, 'neu': 0.0,
+                            'pos': 0.0, 'compound': 0.0}
         # need to keep count because some reviews dont have sentiment (because no reviewText)
         reviewsWithSentimentCount = 0
-        
-        
+
         for rev in reviewsList:
-            
+
             addSentimentToReview(rev, lyzer)
             if 'sentiment' in rev:
                 reviewsWithSentimentCount += 1
                 for key in averageSentiment:
                     averageSentiment[key] += rev['sentiment'][key]
-        
-        
+
         for key in averageSentiment:
             if reviewsWithSentimentCount != 0:
-                averageSentiment[key] = averageSentiment[key] / reviewsWithSentimentCount
-        
+                averageSentiment[key] = averageSentiment[key] / \
+                    reviewsWithSentimentCount
+
         productDocument['averageSentiment'] = averageSentiment
 
     lyzer = SentimentIntensityAnalyzer()
     generateSentiment(testDocument, lyzer)
-    #print(testDocument)
+    # print(testDocument)
 
     # test first doc
     # documents[0]
 
     for document in documents:
         generateSentiment(document, lyzer)
-        
-    ## outputting    
+
+    # outputting
     documentsJsonString = json.dumps(documents, default=json_util.default)
+
+    # pass on to next dag
     # # write list of dicts to file
     with open('outputWithNPCountAndSentiment.json', 'w') as outfile:
         outfile.write(documentsJsonString)
 
-    mydb = client["Cell_Phones"]
-    my_collection = mydb["reviewNPCSentiment"]
+    # mydb = client["Cell_Phones"]
+    # my_collection = mydb["reviewNPCSentiment"]
 
-    with open('outputWithNPCountAndSentiment.json', 'r') as f:
-        data = json.load(f)
+    # with open('outputWithNPCountAndSentiment.json', 'r') as f:
+    #     data = json.load(f)
 
-    all_reviews = []
+    # all_reviews = []
 
-    for record in data:
-        for review in record['reviewList']:
-            all_reviews.append(review)
+    # for record in data:
+    #     for review in record['reviewList']:
+    #         all_reviews.append(review)
 
-    # test print uncomment to view
-    # for i in range(10):
-    #     print(all_reviews[i])
+    # # test print uncomment to view
+    # # for i in range(10):
+    # #     print(all_reviews[i])
 
-    # This will throw an error (duplicate key) but it works
-    converted = loads(json.dumps(all_reviews))
-    my_collection.insert_many(converted)
+    # # This will throw an error (duplicate key) but it works
+    # converted = loads(json.dumps(all_reviews))
+    # my_collection.insert_many(converted)
+
 
 default_args = {
     'owner': 'airflow',
